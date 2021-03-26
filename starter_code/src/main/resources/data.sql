74d2ef3559c597f656b6c63caca647b92d0ccf0c
lafig <lafig@DESKTOP-G57UGNQ.myfiosgateway.com>
2021-03-26 16:36:54 -0400

    Added two more widgets to the data.sql

--- starter_code/src/main/resources/data.sql
+++ starter_code/src/main/resources/data.sql
@@ -1,2 +1,4 @@
 insert into item (name, price, description) values ('Round Widget', 2.99, 'A widget that is round');
 insert into item (name, price, description) values ('Square Widget', 1.99, 'A widget that is square');
+insert into item (name, price, description) values ('Ellipse Widget', 2.99, 'A widget that is in the shape of an Ellipse');
+insert into item (name, price, description) values ('Recoder Widget', 10.99, 'A widget that looks like a recorder');
